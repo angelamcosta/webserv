c3a13c47d8aeec03683972d9a3cd1f292583cee0
Angela Lima <angelamcostalima@icloud.com>
2024-04-17 18:45:35 +0100

    👷 update `header`
    * add new functions
    * add new macros

--- webserv.hpp
+++ webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 13:46:57 by anlima            #+#    #+#             */
-/*   Updated: 2024/04/16 19:11:57 by anlima           ###   ########.fr       */
+/*   Updated: 2024/04/17 18:11:18 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+#include <string>
 
 # define PORT 8080
 # define BUFFER_SIZE 1024
@@ -37,16 +38,27 @@
 # define OK_HEADER "HTTP/1.1 200 OK\r\n"
 # define CONTENT_TYPE "Content-Type: "
 # define CONTENT_LEN "Content-Length: "
+# define CLEAR "\033[0m"
+# define PINK "\033[38;5;206m"
+# define LAVENDER "\033[38;5;183m"
+# define MINT "\033[38;5;121m"
+# define PEACH "\033[38;5;223m"
+# define SKYBLUE "\033[38;5;117m"
+# define YELLOW "\033[38;5;226m"
+# define HEART "♡"
+# define FLOWER "❀"
 
-void        create_process(int server_socket, int client_socket);
+void    create_process(int server_socket, int client_socket);
 
-void        handle_request(int sockfd);
+void    handle_request(int sockfd);
+std::map<std::string, std::string>  get_params(std::string url);
 
-int         bind_socket(int sockfd);
-int         start_server(int sockfd);
-int         create_server_socket(void);
+int bind_socket(int sockfd);
+int start_server(int sockfd);
+int create_server_socket(void);
 
 std::string mime_type(void);
-void        serve_file(int client_socket, std::string const &file_path);
+std::string dynamic_content(std::string content_str, std::map<std::string, std::string> params);
+void    serve_file(int client_socket, std::string const &file_path,std::map<std::string, std::string> params);
 
 #endif
