4b0136429474ba724b9afddff38679b46b385479
Angela Lima <angelamcostalima@icloud.com>
2024-04-17 18:47:57 +0100

    💬 add instructions on how to access the server

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 13:46:03 by anlima            #+#    #+#             */
-/*   Updated: 2024/04/15 17:31:46 by anlima           ###   ########.fr       */
+/*   Updated: 2024/04/17 15:01:59 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int main(void)
 	server_socket = create_server_socket();
 	if (!start_server(server_socket))
 		return (0);
+	std::cout << LAVENDER << HEART << CLEAR << MINT << " Welcome to webserv! " << CLEAR << LAVENDER << HEART << CLEAR << std::endl;
+	std::cout << LAVENDER << "Go to " << CLEAR << SKYBLUE << "localhost:8080" << CLEAR << LAVENDER " to check it out! " << CLEAR << std::endl;
 	while (1)
 	{
 		int client_socket = accept(server_socket, NULL, NULL);
