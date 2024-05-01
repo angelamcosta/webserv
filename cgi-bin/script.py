f5f5ea3d7d6f954d0ea2f3bdbcc5b44ba0da9cbb
Angela Lima <angelamcostalima@icloud.com>
2024-05-01 19:07:27 +0100

    🚧 wip -> python cgi

--- cgi-bin/script.py
+++ cgi-bin/script.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 
-import cgi
+import os
+from urllib.parse import parse_qs
 
-form = cgi.FieldStorage()
-name = form.getvalue('name', 'deafult user')
+query_string = os.environ.get('QUERY_STRING', '')
 
 with open('/home/anlima/webserv/public/index.html', 'r') as file:
     template_content = file.read()
 
-html_content = template_content.replace('{{ name }}', name)
+if not query_string:
+    html_content = template_content.replace('{{ name }}', 'user')
+else:
+    params = parse_qs(query_string)
+    name = params.get('name', ['default user'])[0]
+    html_content = template_content.replace('{{ name }}', name)
 
 print(html_content)
