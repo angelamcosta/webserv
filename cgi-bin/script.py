401fcd5b1a32e024d45a0c42975b2c740e5148df
Angela Lima <angelamcostalima@icloud.com>
2024-05-10 16:58:58 +0100

    🔨 update python CGI script to handle HTTP requests

--- cgi-bin/script.py
+++ cgi-bin/script.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 
+import sys
 import os
-from urllib.parse import parse_qs
 
-query_string = os.environ.get('QUERY_STRING', '')
+url = sys.argv[1];
+method = sys.argv[2];
+filename = sys.argv[3];
+full_path = sys.argv[4];
+error_page = sys.argv[5];
 
-with open('/home/anlima/webserv/public/index.html', 'r') as file:
-    template_content = file.read()
-
-if not query_string:
-    html_content = template_content.replace('{{ name }}', 'user')
+if (os.path.isfile(full_path)):
+    f = open(full_path, "r")
+    print(f.read())
+    f.close()
 else:
-    params = parse_qs(query_string)
-    name = params.get('name', ['default user'])[0]
-    html_content = template_content.replace('{{ name }}', name)
+    f = open(error_page, "r")
+    print(f.read())
+    f.close()
 
-print(html_content)
+# print(url)
+# print(method)
+# print(filename)
+# print(full_path)
+# print(error_page)
