66d55501454781fd829c432c10385134595dec70
Angela Lima <angelamcostalima@icloud.com>
2024-05-17 16:33:16 +0100

    🔨 update `script.py`
    Co-authored-by: Gustavo Silva <gustavocssilva1997@gmail.com>

--- cgi-bin/script.py
+++ cgi-bin/script.py
@@ -3,23 +3,29 @@
 import sys
 import os
 
-url = sys.argv[1];
-method = sys.argv[2];
-filename = sys.argv[3];
-file_path = sys.argv[4];
-error_page = sys.argv[5];
+url = sys.argv[1] if sys.argv[1] != "/" else "/index.html"
+body = sys.argv[2]
+method = sys.argv[3]
+request = sys.argv[4]
+filename = sys.argv[5]
+error_page = sys.argv[6]
+# allowed_methods = sys.argv[7]
+PATH_INFO = "./public"
 
+def generate_response(status, file):
+    print (f"HTTP/1.1 {status} \r\nContent-Length: {len(file)}\r\n\r\n{file}")
+    
 def print_error():
     try:
         with open(error_page, "r") as f:
-            print(f.read())
+            generate_response("404 Not found", f.read())
     except OSError:
-        print("<h1>ERROR: Could not find the specified file</h1>")
+        generate_response("404 Not found", "<h1>ERROR: Could not find the specified file</h1>")
 
-if (os.path.isfile(file_path)):
+if (os.path.isfile(PATH_INFO + url)):
     try:
-        with open(file_path, "r") as f:
-            print(f.read())
+        with open(PATH_INFO + url, "r") as f:
+            generate_response("200 OK", f.read())
     except OSError:
         print_error()
 else:
