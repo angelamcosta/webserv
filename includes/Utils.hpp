34462fee1928902d5f9340f5aa6273b7ceee06a3
Angela Lima <angelamcostalima@icloud.com>
2025-03-12 15:28:12 +0000

    ✨ add `Utils` files
    * after all, we really need to handle requests even if a cgi is not defined

---
+++ includes/Utils.hpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Utils.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 16:14:05 by anlima            #+#    #+#             */
+/*   Updated: 2025/02/06 13:51:48 by anlima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include "macros.hpp"
+
+class Utils
+{
+private:
+    s_request _data;
+    std::string _url;
+    std::string _template;
+    std::string _full_path;
+    std::string _upload_dir;
+    std::string _error_path;
+    Utils();
+
+public:
+    ~Utils();
+    Utils(const s_request &data);
+    Utils(const Utils &copy);
+    Utils &operator=(const Utils &copy);
+
+    const s_request &getData(void);
+
+    const std::string &getUrl(void);
+    void setUrl(void);
+
+    std::string &getTemplate(void);
+    void setTemplate(void);
+
+    std::string &getFullPath(void);
+    void setFullPath(void);
+
+    std::string &getUploadDir(void);
+    void setUploadDir(void);
+
+    std::string &getErrorPath(void);
+    void setErrorPath(void);
+
+    const std::string emptyFolder(void) const;
+    const std::string fileMissing(void) const;
+    const std::string deleteFailed(void) const;
+    const std::string uploadFailed(void) const;
+    const std::string successUpload(void) const;
+    const std::string successDelete(void) const;
+    const std::string generateCards(std::string path_info, std::string url) const;
+
+    void handleMethod(std::string message);
+    void getFile(const std::string &full_path, const std::string &path_info, const std::string &url, const std::string &message, const std::string &status);
+    void generateResponse(const std::string &status, const std::string &content, const std::string &full_path, const std::string &template_str);
+    std::string loadTemplateFile(const std::string &path_info);
+    std::string generateHeaders(const std::string &status, size_t content_length, const std::string &filename);
+    void getDirectories(const std::string &full_path, const std::string &path_info);
+    void getImage(const std::string full_path, const std::string path_info, const std::string error_path, const std::string url, const std::string message, const std::string status);
+    std::string generateHTMLList(const std::vector<std::string> &files);
+};
+
+#endif
---
+++ srcs/Utils.cpp
@@ -0,0 +1,397 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Utils.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anlima <anlima@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 16:31:13 by anlima            #+#    #+#             */
+/*   Updated: 2025/02/06 15:53:08 by anlima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Utils.hpp"
+#include "../includes/Requests.hpp"
+
+Utils::~Utils() {}
+
+Utils::Utils(const s_request &data) : _data(data), _url(""), _full_path(""), _upload_dir(""), _error_path("")
+{
+}
+
+Utils::Utils(const Utils &copy)
+{
+    if (this != &copy)
+    {
+        _data = copy._data;
+        _url = copy._url;
+        _full_path = copy._full_path;
+        _upload_dir = copy._upload_dir;
+        _error_path = copy._error_path;
+    }
+}
+
+Utils &Utils::operator=(const Utils &copy)
+{
+    if (this != &copy)
+    {
+        _data = copy._data;
+        _url = copy._url;
+        _full_path = copy._full_path;
+        _upload_dir = copy._upload_dir;
+        _error_path = copy._error_path;
+    }
+    return (*this);
+}
+
+const s_request &Utils::getData(void) { return (_data); }
+
+const std::string &Utils::getUrl(void) { return (_url); }
+void Utils::setUrl(void)
+{
+    _url = (_data.url == "/") ? '/' + _data.index : _data.index;
+}
+
+std::string &Utils::getTemplate(void) { return (_template); }
+void Utils::setTemplate(void)
+{
+    std::stringstream iss(_data.path_info);
+    iss >> _template;
+}
+
+std::string &Utils::getFullPath(void) { return (_full_path); }
+void Utils::setFullPath(void)
+{
+    if (!_url.empty() && _url[0] == '/' && !_data.path_info.empty() && _data.path_info[_data.path_info.length() - 1] == '/')
+        _full_path = _data.path_info + _url.substr(1);
+    else if ((!_url.empty() && _url[0] == '/') || (!_data.path_info.empty() && _data.path_info[_data.path_info.length() - 1] == '/'))
+        _full_path = _data.path_info + _url;
+    else
+        _full_path = _data.path_info + "/" + _url;
+}
+
+std::string &Utils::getUploadDir(void) { return (_upload_dir); }
+void Utils::setUploadDir(void)
+{
+    if (!_data.path_info.empty())
+    {
+        _upload_dir = _data.path_info[_data.path_info.length() - 1] == '/' ? _data.path_info + "images/" : _data.path_info + "/images/";
+    }
+}
+
+std::string &Utils::getErrorPath(void) { return (_error_path); }
+void Utils::setErrorPath(void)
+{
+    _error_path = _data.path_info + _data.error_page;
+}
+
+const std::string Utils::successUpload(void) const
+{
+    return ("<div class=\"alert alert-success mt-3\" role=\"alert\">\n"
+            "    File uploaded successfully!\n"
+            "</div>\n");
+}
+
+const std::string Utils::uploadFailed(void) const
+{
+    return ("<div class=\"alert alert-danger mt-3\" role=\"alert\">\n"
+            "    File upload failed.\n"
+            "</div>\n");
+}
+
+const std::string Utils::fileMissing(void) const
+{
+    return ("<div class=\"alert alert-warning mt-3\" role=\"alert\">\n"
+            "    No file field found in the form.\n"
+            "</div>\n");
+}
+
+const std::string Utils::successDelete(void) const
+{
+    return ("<div class=\"alert alert-success mt-3\" role=\"alert\">\n"
+            "    File deleted successfully!\n"
+            "</div>\n");
+}
+
+const std::string Utils::deleteFailed(void) const
+{
+    return ("<div class=\"alert alert-danger mt-3\" role=\"alert\">\n"
+            "    File delete failed.\n"
+            "</div>\n");
+}
+
+const std::string Utils::emptyFolder(void) const
+{
+    return ("<div class=\"alert alert-secondary mt-3\" role=\"alert\">\n"
+            "    The pond is empty right now.\n"
+            "</div>\n");
+}
+
+const std::string Utils::generateCards(std::string path_info, std::string url) const
+{
+    std::ostringstream cards;
+    std::vector<std::string> images;
+
+    DIR *dir = opendir(path_info.c_str());
+    if (dir)
+    {
+        struct dirent *entry;
+        while ((entry = readdir(dir)) != NULL)
+        {
+            std::string filename(entry->d_name);
+            if (filename != "." && filename != "..")
+                images.push_back(filename);
+        }
+        closedir(dir);
+    }
+    if (images.empty())
+    {
+        return "<div class=\"alert alert-secondary\" role=\"alert\">\n"
+               "    The pond is empty right now.\n"
+               "</div>\n";
+    }
+    for (std::vector<std::string>::const_iterator it = images.begin(); it != images.end(); ++it)
+    {
+        std::string path = "../images/" + *it;
+        cards << "            <div class=\"col\">\n"
+              << "                <div class=\"card shadow-sm\">\n"
+              << "                    <img src=\"" << path << "\" class=\"card-img-top\" width=\"100%\" height=\"225\" style=\"object-fit: cover;\">\n"
+              << "                    <div class=\"card-body\">\n"
+              << "                        <div class=\"d-flex justify-content-between align-items-center\">\n"
+              << "                            <div class=\"d-grid gap-2 d-md-flex justify-content-md-end\">\n"
+              << "                                <a class=\"btn btn-outline-primary\" href=\"" << path << "\" role=\"button\">View</a>\n"
+              << "                                <form method=\"post\" action=\"" << url << "\">\n"
+              << "                                    <input type=\"hidden\" name=\"_method\" value=\"DELETE\">\n"
+              << "                                    <input type=\"hidden\" name=\"_filename\" value=\"" << *it << "\">\n"
+              << "                                    <button type=\"submit\" class=\"btn btn-outline-secondary\" role=\"button\">Delete</button>\n"
+              << "                                </form>\n"
+              << "                            </div>\n"
+              << "                        </div>\n"
+              << "                    </div>\n"
+              << "                </div>\n"
+              << "            </div>\n";
+    }
+
+    return (cards.str());
+}
+
+bool pathExists(const std::string &path)
+{
+    struct stat buffer;
+    return (stat(path.c_str(), &buffer) == 0);
+}
+
+bool isDirectory(const std::string &path)
+{
+    struct stat buffer;
+    if (stat(path.c_str(), &buffer) == 0)
+        return S_ISDIR(buffer.st_mode);
+    return false;
+}
+
+bool isRegularFile(const std::string &path)
+{
+    struct stat buffer;
+    if (stat(path.c_str(), &buffer) == 0)
+        return S_ISREG(buffer.st_mode);
+    return false;
+}
+
+void Utils::handleMethod(std::string message)
+{
+    if (message == "invalid_size")
+    {
+        message = uploadFailed();
+        getFile(_full_path, _data.path_info, _url, message, "403 Forbidden");
+    }
+    else if (pathExists(_full_path))
+    {
+        if (isDirectory(_full_path))
+        {
+            if (_data.dir_listing == "on")
+                getDirectories(_full_path, _data.path_info);
+            else
+                getFile(_full_path, _data.path_info + "forbidden.html", _url, message, "403 Forbidden");
+        }
+        else if (isRegularFile(_full_path))
+        {
+            if (_full_path.substr(_full_path.size() - 4) == ".png" ||
+                _full_path.substr(_full_path.size() - 4) == ".jpg" ||
+                _full_path.substr(_full_path.size() - 5) == ".jpeg" ||
+                _full_path.substr(_full_path.size() - 4) == ".gif")
+                getImage(_full_path, _data.path_info, _error_path, _url, message, "200 OK");
+            else
+                getFile(_full_path, _data.path_info, _url, message, "200 OK");
+        }
+    }
+    else
+        getFile(_error_path, _data.path_info, _url, message, "404 Not found");
+}
+
+void Utils::getFile(const std::string &full_path, const std::string &path_info, const std::string &url,
+                  const std::string &message = "", const std::string &status = "200 OK")
+{
+    std::ifstream file(full_path.c_str());
+    if (!file.is_open())
+    {
+        generateResponse("404 Not Found",
+                         "<h1>ERROR: Could not find the specified file</h1>",
+                         full_path, loadTemplateFile(path_info));
+        return;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string text = buffer.str();
+    file.close();
+
+    size_t pos;
+    while ((pos = text.find("{{alert}}")) != std::string::npos)
+        text.replace(pos, 9, message);
+
+    if ((pos = text.find("{{images}}")) != std::string::npos)
+    {
+        std::string cards = generateCards(path_info + "images", url);
+        text.replace(pos, 10, cards);
+    }
+
+    generateResponse(status, text, full_path, loadTemplateFile(path_info));
+}
+
+std::string Utils::loadTemplateFile(const std::string &path_info)
+{
+    std::string template_path = path_info;
+    if (!template_path.empty() && template_path[template_path.size() - 1] == '/')
+        template_path += "template.html";
+    else
+        template_path += "/template.html";
+
+    std::ifstream file(template_path.c_str());
+    if (!file.is_open())
+        return "";
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+
+    return (buffer.str());
+}
+
+std::string Utils::generateHeaders(const std::string &status, size_t content_length, const std::string &filename)
+{
+    std::ostringstream headers;
+    headers << "HTTP/1.1 " << status << "\r\n"
+            << "Content-Length: " << content_length << "\r\n"
+            << "Content-Disposition: inline; filename=\"" << filename << "\"\r\n"
+            << "Content-Type: text/html\r\n"
+            << "\r\n";
+    return (headers.str());
+}
+
+void Utils::generateResponse(const std::string &status, const std::string &content, const std::string &fullPath, const std::string &templateStr = "")
+{
+    std::string updated_template = templateStr;
+    std::string placeholder = "{{placeholder}}";
+    size_t pos = updated_template.find(placeholder);
+    if (pos != std::string::npos)
+        updated_template.replace(pos, placeholder.length(), content);
+    size_t slashPos = fullPath.find_last_of("/\\");
+    std::string filename = (slashPos != std::string::npos) ? fullPath.substr(slashPos + 1) : fullPath;
+    size_t contentLength = updated_template.size();
+    std::string headers = generateHeaders(status, contentLength, filename);
+    std::cout << headers << updated_template << std::endl;
+}
+
+void Utils::getDirectories(const std::string &full_path, const std::string &path_info)
+{
+    std::vector<std::string> files;
+    DIR *dir = opendir(full_path.c_str());
+    if (dir != NULL)
+    {
+        struct dirent *entry;
+        while ((entry = readdir(dir)) != NULL)
+        {
+            std::string name = entry->d_name;
+            if (name != "." && name != "..")
+                files.push_back(name);
+        }
+        closedir(dir);
+    }
+    else
+    {
+        std::cerr << "Error: Unable to open directory " << full_path << std::endl;
+        return;
+    }
+
+    std::string template_file = loadTemplateFile(path_info);
+    std::string file_list_HTML = generateHTMLList(files);
+    std::string html_code =
+        "<div class=\"container\">"
+        "<div class=\"row\">"
+        "<div class=\"col-md-12\">"
+        "<div class=\"error-template\">"
+        "<h1>" +
+        full_path + "</h1>"
+                    "<div class=\"error-details\">" +
+        file_list_HTML +
+        "</div>"
+        "</div>"
+        "</div>"
+        "</div>"
+        "</div>";
+
+    generateResponse("200 OK", html_code, path_info + "index.html", template_file);
+}
+
+std::string Utils::generateHTMLList(const std::vector<std::string> &files)
+{
+    std::ostringstream html_stream;
+    html_stream << "<ul>";
+    for (size_t i = 0; i < files.size(); ++i)
+        html_stream << "<li>" << files[i] << "</li>";
+    html_stream << "</ul>";
+    return (html_stream.str());
+}
+
+void Utils::getImage(const std::string full_path, const std::string path_info, const std::string error_path, const std::string url, const std::string message, const std::string status)
+{
+    std::ifstream file(full_path.c_str(), std::ios::in | std::ios::binary);
+    if (!file.is_open())
+    {
+        getFile(error_path, path_info, url, message, "404 Not Found");
+        return;
+    }
+
+    struct stat file_stat;
+    if (stat(full_path.c_str(), &file_stat) != 0)
+    {
+        getFile(error_path, path_info, url, message, "404 Not Found");
+        return;
+    }
+    size_t contentLength = file_stat.st_size;
+
+    char *content = new char[contentLength];
+    file.read(content, contentLength);
+    if (!file || file.gcount() != static_cast<std::streamsize>(contentLength))
+    {
+        delete[] content;
+        getFile(error_path, path_info, url, message, "404 Not Found");
+        return;
+    }
+
+    file.close();
+
+    std::string filename;
+    size_t pos = full_path.find_last_of("/\\");
+    if (pos != std::string::npos)
+        filename = full_path.substr(pos + 1);
+    else
+        filename = full_path;
+
+    std::string headers = generateHeaders(status, contentLength, filename);
+
+    std::cout << headers << "\r\n";
+    std::cout.write(content, contentLength);
+
+    delete[] content;
+}
